c074a42380b89cc99cc660fa1d90663b079e1727
Your Name <your_email@whatever.com>
2015-10-31 12:25:56 +0200

    v0.2.1
    DOM build on JS

--- script.js
+++ script.js
@@ -1,60 +1,92 @@
 var square = document.getElementsByClassName("square");
 var num_ship = 10;
+var fields = 25;
 
-$('.square').click(function() {
-  $(this).addClass('red');
-  var num = $('div.red').length;
-  
-  //if bingo
-  if($('.green.red').length >= num_ship) {
-    $('.message').fadeIn(600);
-    $('.mes-inner').fadeIn(600);
-    
-    if (num > num_ship) {
-      $('.mes-inner').html("You win!!! It takes " + num + " attepts!");
-    }
-    else {
-      $('.mes-inner').html("You win!!! You are very lucky!!!");
-    }
-    return false;
-    button_click();
-  }
-});
-
-$('button').click(function() {
-  button_click();
-})
 $(document).ready(function() {
-  $('button').click();
-  $('.message').click(function() {
-    $(this).fadeOut(600);
-    button_click();
-  })
-})
+    //add empty fields to our game
+    addEmptyFields(fields);
 
-//actions when button is clicked
-function button_click() {
-  $('button').text("New Game!");
-  $('.red, .green').removeClass('red green');
-  var rand_sq = random(0, square.length, num_ship);
-  for (var l = 0; l < rand_sq.length; l++) {
-    var k = rand_sq[l];
-    $('.square:eq(' + k + ')').addClass('green');
-  }
-}
+    startNewGame();
+    //when click on message this close
+    // $('.message').click(function() {
+    //     $(this).fadeOut(600);
+    //     startNewGame();
+    // })
+
+    $('.square').click(function() {
+        $(this).addClass('red');
+
+        //change number of destroyed ships after clicking square
+        destroyedShipsNum();
+
+        //when game is finished display message
+        displayTotalMessage();
+    });
+
+    $('button').click(function() {
+        $('.message').fadeOut(600);
+        startNewGame();
+    });
+
+    //add empty fields to our game
+    function addEmptyFields(fields) {
+      var divSquare = '<div class="square"></div>';
+      for (var i = 0; i < fields; i++) {
+          $('button').before(divSquare);
+      };
+    }
+
+    //actions when button is clicked
+    //to start new game
+    function startNewGame() {
+        $('button').text("New Game!");
+        $('.red, .green').removeClass('red green');
+        var rand_sq = random(0, square.length, num_ship);
+        for (var l = 0; l < rand_sq.length; l++) {
+            var k = rand_sq[l];
+            $('.square:eq(' + k + ')').addClass('green');
+        }
+        //show number of destroyed ships
+        destroyedShipsNum();
+    }
+
+    //show number of destroyed ships
+    function destroyedShipsNum() {
+        var destroyedShips = $('.green.red').length;
+        var messageDestroyedShips = "Destroyed ships: " + destroyedShips + "/" + num_ship;
+        $('.count-message').text(messageDestroyedShips);
+    }
+
+    //when game is finished display message
+    function displayTotalMessage() {
+      var num = $('div.red').length;
+      if ($('.green.red').length >= num_ship) {
+        $('.message').fadeIn(600);
+        $('.mes-inner').fadeIn(600);
+
+        if (num > num_ship) {
+            $('.mes-inner').html("You win!!! It takes " + num + " attepts!");
+        } else {
+            $('.mes-inner').html("You win!!! You are very lucky!!!");
+        }
+      }
+    }
+})
 
 //generation of n different random numbers
 function random(min, max, n) {
-  if (max - min < n) {return false;}
-  var result = [];
-  var arr = [];
-  for (var j = min; j < max; j++) {
-    arr[j] = j;
-  }
-  for (var i = 0; i < n; i++) {
-    var random_number = Math.floor(Math.random() * arr.length + min);
-    result[i] = " " + arr[random_number];
-    arr.splice(random_number, 1);
-  }
-  return result;
+    if (max - min < n) {
+        return false;
+    }
+    var result = [];
+    var arr = [];
+    for (var j = min; j < max; j++) {
+        arr[j] = j;
+    }
+    for (var i = 0; i < n; i++) {
+        var random_number = Math.floor(Math.random() * arr.length + min);
+        result[i] = " " + arr[random_number];
+        arr.splice(random_number, 1);
+    }
+    return result;
 }
