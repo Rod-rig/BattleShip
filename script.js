7b6d485abf449d8427fa054d67ef725a37e378f3
Your Name <your_email@whatever.com>
2015-10-31 12:22:48 +0200

    v0.2
    version 0.2

--- script.js
+++ script.js
@@ -1,34 +1,60 @@
+var square = document.getElementsByClassName("square");
+var num_ship = 10;
+
 $('.square').click(function() {
   $(this).addClass('red');
   var num = $('div.red').length;
-  if($(this).hasClass('green red')) {
+  
+  //if bingo
+  if($('.green.red').length >= num_ship) {
     $('.message').fadeIn(600);
     $('.mes-inner').fadeIn(600);
     
-    if (num > 1) {
+    if (num > num_ship) {
       $('.mes-inner').html("You win!!! It takes " + num + " attepts!");
     }
     else {
       $('.mes-inner').html("You win!!! You are very lucky!!!");
     }
     return false;
-    $('button').click();
+    button_click();
   }
 });
 
-var square = document.getElementsByClassName("square");
-
 $('button').click(function() {
-  $('button').text("New Game!");
-  $('.red').removeClass('red green');
-  var j = Math.floor(Math.random() * $('.square').length);
-  square[j].className += ' green';
+  button_click();
 })
-
 $(document).ready(function() {
   $('button').click();
   $('.message').click(function() {
     $(this).fadeOut(600);
-    $('button').click();
+    button_click();
   })
 })
+
+//actions when button is clicked
+function button_click() {
+  $('button').text("New Game!");
+  $('.red, .green').removeClass('red green');
+  var rand_sq = random(0, square.length, num_ship);
+  for (var l = 0; l < rand_sq.length; l++) {
+    var k = rand_sq[l];
+    $('.square:eq(' + k + ')').addClass('green');
+  }
+}
+
+//generation of n different random numbers
+function random(min, max, n) {
+  if (max - min < n) {return false;}
+  var result = [];
+  var arr = [];
+  for (var j = min; j < max; j++) {
+    arr[j] = j;
+  }
+  for (var i = 0; i < n; i++) {
+    var random_number = Math.floor(Math.random() * arr.length + min);
+    result[i] = " " + arr[random_number];
+    arr.splice(random_number, 1);
+  }
+  return result;
+}
